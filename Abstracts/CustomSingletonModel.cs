580a71f572aa121f55bf6f9e30ec423467b4eb89
Syra 3 <25936043+syralessthanthree@users.noreply.github.com>
2026-03-31 03:49:26 +0200

    Got it working but this is cursed.

--- Abstracts/CustomSingletonModel.cs
+++ Abstracts/CustomSingletonModel.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System.Reflection;
+using HarmonyLib;
 using MegaCrit.Sts2.Core.Modding;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Exceptions;
@@ -12,32 +13,69 @@ public abstract class CustomSingletonModel : SingletonModel {
 
     public abstract SingletonSettings registerSettings { get; }
     public abstract string modId { get; }
+    
+    
 
-    protected CustomSingletonModel() {
+    public static void RegisterSingletonType(string modIdToRegisterWith, Type type) {
+        CustomSingletonModel? singletonInstance = System.Activator.CreateInstance(type) as CustomSingletonModel;
+        if (singletonInstance == null) {
+            throw new TypeLoadException();
+        }
+        var modId = singletonInstance.modId;
+        var Id = singletonInstance.Id;
+        
+        if(!modIdToRegisterWith.Equals(modId))return;
+        BaseLibMain.Logger.Info($"Trying to register singleton:{type} with ModID:{modId}");
+        
+        if (ModelDb.Contains(type))
+            throw new DuplicateModelException(type);
+        if(modelsToRegister.Values.Any((i)=> i.Any((i2)=>i2.Equals(Id))))
+            throw new DuplicateModelException(type);
+        
         if (modelsToRegister.ContainsKey(modId)) {
-            modelsToRegister[modId].Add(ModelDb.GetById<CustomSingletonModel>(Id));
+            modelsToRegister[modId].Add(GetSingletonId(Id,type));
         } else {
-            modelsToRegister.Add(modId,[ModelDb.GetById<CustomSingletonModel>(Id)]);
+            modelsToRegister.Add(modId,[GetSingletonId(Id,type)]);
         }
     }
-    
-    public static CustomSingletonModel GetSingletonId<T>(ModelId id) where T : CustomSingletonModel{
-        if (!ModelDb.Contains(typeof(T)))
-            ModelDb.Inject(typeof(T));
-        return ModelDb.GetByIdOrNull<T>(id) ?? throw new ModelNotFoundException(id);
+
+    protected CustomSingletonModel() {
+        
+    }
+
+    public static ModelId GetSingletonId(ModelId id, Type type) {
+        return id;
     }
     
-    private static Dictionary<string, List<CustomSingletonModel>> modelsToRegister = []; 
+    private static Dictionary<string, List<ModelId>> modelsToRegister = []; 
     
     public struct SingletonSettings {
         public bool SubscribeToRunStateHooks;
         public bool SubscribeToCombatStateHooks;
     }
-    public static void Subscribe(string ModID)
-    {
-        if(!modelsToRegister.ContainsKey(ModID)) return;
+    public static void Subscribe(string ModID) {
+
+        if (!modelsToRegister.ContainsKey(ModID)) {
+            var classesToRegister = Assembly.GetExecutingAssembly().GetTypes().ToList()
+                .FindAll((t) => t.IsSubclassOf(typeof(CustomSingletonModel)));
+            foreach (var type in classesToRegister) {
+                RegisterSingletonType(ModID,type);
+            }
+        }
+        if (!modelsToRegister.ContainsKey(ModID)) return;
+            
         var ourModels = modelsToRegister[ModID];
-        ModHelper.SubscribeForRunStateHooks(ModID,(_) => ourModels.FindAll((m)=> m.registerSettings.SubscribeToRunStateHooks));
-        ModHelper.SubscribeForCombatStateHooks(ModID,(_) => ourModels.FindAll((m)=> m.registerSettings.SubscribeToCombatStateHooks));
+        ModHelper.SubscribeForRunStateHooks(ModID,(_) => IdToSubscription(_,ourModels,
+            (m)=>m.registerSettings.SubscribeToRunStateHooks)
+        );
+        ModHelper.SubscribeForCombatStateHooks(ModID,(_) => IdToSubscription(_,ourModels,
+            (m)=>m.registerSettings.SubscribeToCombatStateHooks)
+        );
+    }
+
+    protected static IEnumerable<AbstractModel> IdToSubscription(object _, List<ModelId> ids, Func<CustomSingletonModel,bool> hook) {
+        var models = ids.FindAll((id) => hook(ModelDb.GetById<CustomSingletonModel>(id)))
+        .ConvertAll((id) => ModelDb.GetById<CustomSingletonModel>(id));
+        return models;
     }
 }
